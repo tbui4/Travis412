f731095583c81909a84ab96a15ddac913805d293
tbui4 <126439693+tbui4@users.noreply.github.com>
2023-02-27 03:50:32 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,10 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	
+	@Test
+	public void testMinusError() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(5, 3), 2);
+	}
 }
