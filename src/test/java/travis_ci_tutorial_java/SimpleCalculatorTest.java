c323c54084780dbca933878d28b97f342f741bfd
tbui4 <126439693+tbui4@users.noreply.github.com>
2023-02-27 13:18:01 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,28 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+	
+		@Test 
+	public void testMultError() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(4, 4), 16);
+	}
+	
+	@Test
+	public void testMultError1() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(4, 4), 16);
+	}
+	
+	@Test 
+	public void testDivideError() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(8, 4), 2);
+	}
+	
+	@Test
+	public void testDivideError1() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(8, 4), 2);
+	}
 }
